3526b473802d3fa0e1a9a38c498038705ce81421
Abhay Patidar <abhaypatidar1718@gmail.com>
2024-07-25 15:20:37 +0530

    frontend

--- src/services/apiConnector.jsx
+++ src/services/apiConnector.jsx
@@ -1,15 +1,22 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({});
+// Create a base instance of axios
+export const axiosInstance = axios.create({
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+});
 
-
+// Function to make API requests
 export const apiConnector = (method, url, bodyData, headers, params) => {
-    console.log('url-------------------------------->',url)
-    return axiosInstance({
-        method: method,
-        url: url,
-        data: bodyData?bodyData:null,
-        headers: headers?headers:null,
-        params: params?params:null
-    })
+  console.log('url-------------------------------->', url);
+  return axiosInstance({
+    method: method,
+    url: url,
+    data: bodyData ? bodyData : null,
+    headers: headers ? headers : null,
+    params: params ? params : null
+  });
 };
